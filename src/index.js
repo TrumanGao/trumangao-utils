d19450c6f3d0a5a08729fd28249907dacf619e0e
gaofeixiang <gaofeixiang@kanyun.com>
2023-07-28 16:14:01 +0800

    delete files

--- src/index.js
+++
@@ -1,6 +0,0 @@
-const data = require("./utils/data.js");
-const regex = require("./utils/regex.js");
-const env = require("./utils/env.js");
-const index = require("./utils/index.js");
-
-module.exports = Object.assign(data, regex, env, index);
