9c146ee15657dcaaeec89adc7c5d7f3dd927efeb
gaofeixiang <gaofeixiang@kanyun.com>
2023-09-26 15:50:21 +0800

    update code comment of EventManager

--- src/utils/eventListener.ts
+++ src/utils/eventListener.ts
@@ -1,79 +1,75 @@
 import { EventEmitter } from "events";
 
 export type EventManagerMapType = Map<
-  EventEmitter | EventTarget | Window | Document,
+  Window | Document | EventTarget | EventEmitter,
   {
+    [eventName in keyof WindowEventMap]?: {
+      [eventListenerKey: string]: EventListenerOrEventListenerObject;
+    };
+  } & {
+    [eventName in keyof DocumentEventMap]?: {
+      [eventListenerKey: string]: EventListenerOrEventListenerObject;
+    };
+  } & {
     [eventName: string]: {
-      [eventListenerKey: string]: (...args: any[]) => void;
-    };
-  } & {
-    [eventNameW in keyof WindowEventMap]?: {
-      [eventListenerKey: string]: (...args: any[]) => void;
-    };
-  } & {
-    [eventNameD in keyof DocumentEventMap]?: {
       [eventListenerKey: string]: (...args: any[]) => void;
     };
   }
 >;
 
 /**
- * Managing events on window, document, EventTarget or node EventEmitter.
+ * Managing events on window, document, EventTarget(Web), EventEmitter(Node).
+ * The instance exposes the eventManagerMap property and addEventListener / removeEventListener methods
+ * to manage the memory address of the eventListener on the purpose of avoiding memory leaks.
  *
- * The instance exposes the eventManagerMap property and the addEventListener and removeEventListener methods
- * to manage the memory address of the eventListener to avoid memory leaks.
- *
- * window, document 等 EventTarget 和 node EventEmitter 事件管理器。
- *
- * 实例暴露 eventManagerMap 属性和 addEventListener 和 removeEventListener 方法，
+ * window, document, EventTarget(Web), EventEmitter(Node) 事件侦听管理器。
+ * 实例暴露 eventManagerMap 属性和 addEventListener / removeEventListener 方法，
  * 用于管理 eventListener 的内存地址，避免内存泄漏
  */
 export class EventManager {
+  /**
+   * @example { Window: { resize: { window_resize_handleResize: handleResize } }
+   */
   static eventManagerMap: EventManagerMapType = new Map();
 
   /**
-   * @param eventName 与 window.addEventListener 或 document.addEventListener 一致
-   * @param eventListener 与 window.addEventListener 或 document.addEventListener 一致
-   * @param eventListenerKey eventListener 的唯一标识，removeEventListener 时删除。命名参考: eventEmitter_eventName_fileName_functionName
-   * @param eventEmitter "window" | "document"，默认 "window"
-   * @example addEventListener("resize", this.handleResize, "window_resize_fileName_handleResize", "window")
-   * @example addEventListener("visibilitychange", this.handleVisibilitychange, "document_visibilitychange_fileName_handleVisibilitychange", "document")
+   * @param eventName 事件名称。WindowEventMap / DocumentEventMap / 其他 EventTarget 或 EventEmitter 支持监听的事件
+   * @param eventListener EventTarget / EventEmitter 支持的事件侦听器
+   * @param eventListenerKey eventListener 的唯一标识，removeEventListener 时删除。命名参考: eventEmitter_eventName_functionName
+   * @param eventEmitter Window / Document / EventTarget / EventEmitter, 默认 Window
+   * @example addEventListener("resize", handleResize, "window_resize_handleResize", Window)
    */
   addEventListener(
-    eventName: string,
-    eventListener: (...args: any[]) => void,
-    eventListenerKey: string,
-    eventEmitter: EventEmitter,
-  ): EventManagerMapType;
-  addEventListener(
-    eventName: string,
+    eventName: keyof WindowEventMap,
     eventListener: EventListenerOrEventListenerObject,
-    eventListenerKey: string,
-    eventEmitter: EventTarget,
-  ): EventManagerMapType;
-  addEventListener(
-    eventName: string | keyof WindowEventMap,
-    eventListener: (...args: any[]) => void,
     eventListenerKey: string,
     eventEmitter: Window,
   ): EventManagerMapType;
   addEventListener(
-    eventName: string | keyof DocumentEventMap,
-    eventListener: (...args: any[]) => void,
+    eventName: keyof DocumentEventMap,
+    eventListener: EventListenerOrEventListenerObject,
     eventListenerKey: string,
     eventEmitter: Document,
   ): EventManagerMapType;
   addEventListener(
-    eventName: string | keyof WindowEventMap | keyof DocumentEventMap,
+    eventName: string,
     eventListener: (...args: any[]) => void,
     eventListenerKey: string,
-    eventEmitter: EventEmitter | EventTarget | Window | Document = window,
+    eventEmitter: EventTarget | EventEmitter,
+  ): EventManagerMapType;
+  addEventListener(
+    eventName: keyof WindowEventMap | keyof DocumentEventMap | string,
+    eventListener: (...args: any[]) => void,
+    eventListenerKey: string,
+    eventEmitter: Window | Document | EventTarget | EventEmitter = window,
   ) {
     if (
-      !(eventEmitter instanceof EventTarget) &&
-      !(eventEmitter instanceof EventEmitter)
+      !(
+        eventEmitter instanceof EventTarget ||
+        eventEmitter instanceof EventEmitter
+      )
     ) {
-      throw new Error("eventEmitter 参数类型错误，无法注册事件侦听器");
+      throw new Error("参数 eventEmitter 类型错误，无法注册事件侦听器");
     }
 
     this.removeEventListener(eventName, eventListenerKey, eventEmitter as any);
@@ -99,49 +95,46 @@ export class EventManager {
 
     console.log(
       `eventManagerMap 新增 ${eventListenerKey}: `,
-      EventManager.eventManagerMap,
+      eventNameMap[eventName][eventListenerKey],
     );
 
     return EventManager.eventManagerMap;
   }
 
   /**
-   * @param eventName 与 window.addEventListener 或 document.addEventListener 一致
-   * @param eventListenerKey eventListener 的唯一标识，addEventListener 时添加。命名参考: eventEmitter_eventName_fileName_functionName
-   * @param eventEmitter "window" | "document"，默认 "window"
+   * @param eventName 事件名称。WindowEventMap / DocumentEventMap / 其他 EventTarget 或 EventEmitter 支持监听的事件
+   * @param eventListenerKey eventListener 的唯一标识，removeEventListener 时删除。命名参考: eventEmitter_eventName_functionName
+   * @param eventEmitter Window / Document / EventTarget / EventEmitter, 默认 Window
    * @example removeEventListener("resize", "window_resize_fileName_handleResize", "window")
-   * @example removeEventListener("visibilitychange", "document_visibilitychange_fileName_handleVisibilitychange", "document")
+   * @example addEventListener("resize", handleResize, "window_resize_handleResize", Window)
    */
   removeEventListener(
-    eventName: string,
-    eventListenerKey: string,
-    eventEmitter: EventEmitter,
-  ): EventManagerMapType | void;
-  removeEventListener(
-    eventName: string,
-    eventListenerKey: string,
-    eventEmitter: EventTarget,
-  ): EventManagerMapType | void;
-  removeEventListener(
-    eventName: string | keyof WindowEventMap,
+    eventName: keyof WindowEventMap,
     eventListenerKey: string,
     eventEmitter: Window,
   ): EventManagerMapType | void;
   removeEventListener(
-    eventName: string | keyof DocumentEventMap,
+    eventName: keyof DocumentEventMap,
     eventListenerKey: string,
     eventEmitter: Document,
   ): EventManagerMapType | void;
   removeEventListener(
-    eventName: string | keyof WindowEventMap | keyof DocumentEventMap,
+    eventName: string,
     eventListenerKey: string,
-    eventEmitter: EventEmitter | EventTarget | Window | Document = window,
+    eventEmitter: EventTarget | EventEmitter,
+  ): EventManagerMapType | void;
+  removeEventListener(
+    eventName: keyof WindowEventMap | keyof DocumentEventMap | string,
+    eventListenerKey: string,
+    eventEmitter: Window | Document | EventTarget | EventEmitter = window,
   ) {
     if (
-      !(eventEmitter instanceof EventTarget) &&
-      !(eventEmitter instanceof EventEmitter)
+      !(
+        eventEmitter instanceof EventTarget ||
+        eventEmitter instanceof EventEmitter
+      )
     ) {
-      throw new Error("eventEmitter 参数类型错误，无法移除事件侦听器");
+      throw new Error("参数 eventEmitter 类型错误，无法移除事件侦听器");
     }
 
     const eventNameMap = EventManager.eventManagerMap.get(eventEmitter) || {};
@@ -167,7 +160,7 @@ export class EventManager {
 
     console.log(
       `eventManagerMap 删除 ${eventListenerKey}: `,
-      EventManager.eventManagerMap,
+      eventNameMap[eventName],
     );
 
     return EventManager.eventManagerMap;
